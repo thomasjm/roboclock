8612de3ea5b125de8a8e6d465b492050aae47695
Tom McLaughlin <thomasjm@stanford.edu>
2014-11-16 02:05:37 -0800

    Equal arm lengths seem good

--- Clock.hs
+++ Clock.hs
@@ -2,24 +2,64 @@ module Clock where
 
 import Diagrams.Prelude
 import Rendering (r)
+import Util
 
+import Data.Maybe
+import Control.Monad
+
+import Debug.Trace
+
+-- Settings
 l1 = 10
-l2 = 5
+l2 = 10
+s = 20
 
-motor direction = arrow # fc blue
-                  <>
-                  circ # lc blue
-                      where
-    arrow = arrowAt origin end1
-    end1 = l1 *^ fromDirection direction
-    circ = translate end1 (circle l2)
 
-bothMotors dir1 dir2 = position
-                       [(origin, motor dir1),
-                        (translateX 20 origin, motor dir2)]
+moveCircle p c = translate (r2 . unp2 $ p) c
 
-data Circle = Circle { circOrigin :: P2
-                     , circRadius :: Double }
+data DrawingInfo = DrawingInfo { motor1Origin :: P2
+                               , motor2Origin :: P2
+                               , end1 :: P2
+                               , end2 :: P2
+                               , pen :: Maybe P2 } deriving Show
 
-topIntersectionOfCircles :: Circle -> Circle -> Maybe R2
-topIntersectionOfCircles = undefined
+bothMotors (DrawingInfo motor1Origin motor2Origin end1 end2 pen) =
+    penAndArrows <>
+    arrowBetween motor1Origin end1 # lc white <>
+    arrowBetween motor2Origin end2 # lc white <>
+    moveCircle end1 (circle l2) # lc blue <>
+    moveCircle end2 (circle l2) # lc blue
+        where penAndArrows = case pen of Nothing -> mempty
+                                         Just p -> arrowBetween end1 p # lc green <>
+                                                   arrowBetween end2 p # lc green
+
+getDrawingInfo theta1 theta2 = DrawingInfo motor1Origin motor2Origin end1 end2 pen where
+    motor1Origin = origin
+    motor2Origin = translateX s motor1Origin
+
+    end1 = motor1Origin .+^ (l1 *^ fromDirection theta1)
+    end2 = motor2Origin .+^ (l1 *^ fromDirection theta2)
+
+    pen = topIntersectionOfCircles (Circle end1 l2) (Circle end2 l2)
+
+
+drawSweep n = (foldl1 (<>) $ map (flip moveCircle $ (circle 0.05 # fc yellow # lc yellow)) validPoints)
+           <> moveCircle m1origin (circle 0.5 # fc red)
+           <> moveCircle m2origin (circle 0.5 # fc red)
+           <> bothMotors firstValid
+
+    where
+      sweepInfos = [getDrawingInfo (theta1 @@ deg) (theta2 @@ deg) | theta1 <- (linspace 0 360 n), theta2 <- (linspace 0 360 n)]
+      validPoints = catMaybes (map pen sweepInfos)
+
+      firstValid = head $ filter (isJust . pen) sweepInfos
+      m1origin = motor1Origin firstValid
+      m2origin = motor2Origin firstValid
+
+
+drawSweep' n =
+    foldl1 (<>) $ map bothMotors validInfos
+
+    where
+      sweepInfos = [getDrawingInfo (theta1 @@ deg) (theta2 @@ deg) | theta1 <- (linspace 0 360 n), theta2 <- (linspace 0 360 n)]
+      validInfos = filter (isJust . pen) sweepInfos
---
+++ Util.hs
@@ -0,0 +1,36 @@
+module Util where
+
+import Diagrams.Prelude
+
+import Diagrams.TwoD.Vector
+
+import Data.List (maximumBy)
+import Data.Function (on)
+
+import Debug.Trace
+
+data Circle = Circle { circOrigin :: P2
+                     , circRadius :: Double } deriving Show
+
+-- Only works on equal-radius circles
+topIntersectionOfCircles :: Circle -> Circle -> Maybe P2
+topIntersectionOfCircles c1@(Circle p1 radius1) c2@(Circle p2 radius2)
+    | magnitude (2*d) > radius1 + radius2 = Nothing
+    | otherwise = Just pen
+    where
+      d = (p2 .-. p1) / 2
+
+      -- We know the intersection we want satisfies
+      -- |d|**2 + |p|**2 = radius1**2
+      h = sqrt $ radius1**2 - (magnitude d)**2
+      v = h *^ (normalized $ perp d)
+
+      -- Take the one with the larger y-value
+      pen = maximumBy (compare `on` (snd . unp2)) [p1 .+^ (d + v), p1 .+^ (d - v)]
+
+-- Floating a => a -> a -> a -> a
+linspace :: Double -> Double -> Int -> [Double]
+linspace start end num = map (\x -> start + fromIntegral x * dx) [0..(num-1)] where
+    dx = ((end - start) / fromIntegral (num-1))
+
+    -- scanl (+) start (replicate num ((end - start) / fromIntegral num))
