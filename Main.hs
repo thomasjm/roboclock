0c2644630aeb78cd6913c5cc7321dc5fbd329f2b
Tom McLaughlin <tomm@dropbox.com>
2014-12-24 18:12:58 -0800

    Working on profiling

--- Main.hs
+++ Main.hs
@@ -6,4 +6,5 @@ import Clock
 import Rendering
 
 
-main = r $ bothMotors $ getDrawingInfo (3,3,3,3) (50 @@ deg) (130 @@ deg)
+-- main = r $ bothMotors $ getDrawingInfo (3,3,3,3) (50 @@ deg) (130 @@ deg)
+main = renderFrameList $ makeLettersAnimation (3,9,3,9)
